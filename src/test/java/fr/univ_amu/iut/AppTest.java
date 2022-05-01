c10d161692d82b5b474521f1627f015f0925fedc
Sébastien NEDJAR <sebastien.nedjar@univ-amu.fr>
2022-05-01 08:43:15 +0000

    Add config for dev container

--- src/test/java/fr/univ_amu/iut/AppTest.java
+++ src/test/java/fr/univ_amu/iut/AppTest.java
@@ -2,7 +2,6 @@ package fr.univ_amu.iut;
 
 import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.ApplicationTest;
@@ -10,7 +9,6 @@ import org.testfx.framework.junit5.ApplicationTest;
 import static org.testfx.api.FxAssert.verifyThat;
 import static org.testfx.util.NodeQueryUtils.hasText;
 
-@Disabled
 @ExtendWith(ApplicationExtension.class)
 public class AppTest extends ApplicationTest {
 
